ae879aee0d9538b86ee09c3de196ce18feb12032
민영 <104131962+y0ungg@users.noreply.github.com>
2022-09-23 10:33:41 +0900

    Style: UI update for Mypage PrevChallenges (#59)

--- client/src/views/components/mypage/MypageSettings.tsx
+++ client/src/views/components/mypage/MypageSettings.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Title } from './mypageStyle';
 
 const MypageSettings = () => {
   return (
-        <></>
+    <>
+    <Title>내 정보 수정</Title>
+    <>form 들어올 자리</>
+    <Exit>선인장 키우기를 떠나실 건가요?</Exit>
+    </>
   );
 };
+
+const Exit = styled.button`
+background: none;
+font-size: 1rem;
+width: fit-content;
+text-align: right;
+`;
 
 export default MypageSettings;
