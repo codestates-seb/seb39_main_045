bc3c73186648a6c50748645eb7a2c6f751367381
romcanrom <baros2san@gmail.com>
2022-09-27 10:56:46 +0900

    feat: cors config fix

--- server/server/src/main/java/com/cactusvilleage/server/auth/web/config/CorsConfig.java
+++ server/server/src/main/java/com/cactusvilleage/server/auth/web/config/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("https://cactus-villeage.com/");
+//        config.addAllowedOriginPattern("https://*.cactus-villeage.com/");
+        config.addAllowedOriginPattern("*");
 
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
