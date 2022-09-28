21e8cc18f7ede096956ebf84d12779f56c27144c
jphwany <pjh5762@naver.com>
2022-09-28 22:05:37 +0900

    fix: git branch conflict

--- server/server/src/main/java/com/cactusvilleage/server/challenge/service/S3Upload.java
+++ server/server/src/main/java/com/cactusvilleage/server/challenge/service/S3Upload.java
@@ -2,8 +2,6 @@ package com.cactusvilleage.server.challenge.service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.cactusvilleage.server.challenge.entities.History;
-import com.cactusvilleage.server.challenge.repository.HistoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
