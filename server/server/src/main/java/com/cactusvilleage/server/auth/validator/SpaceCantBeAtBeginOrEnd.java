011cd57295494d843ab5ecca8b2bdad74101e0db
romcanrom <baros2san@gmail.com>
2022-09-28 23:25:33 +0900

    feat: username validator added for auth

---
+++ server/server/src/main/java/com/cactusvilleage/server/auth/validator/SpaceCantBeAtBeginOrEnd.java
@@ -0,0 +1,17 @@
+package com.cactusvilleage.server.auth.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = SpaceValidator.class)
+public @interface SpaceCantBeAtBeginOrEnd {
+    String message() default "닉네임 앞뒤로는 공백이 들어갈 수 없습니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
