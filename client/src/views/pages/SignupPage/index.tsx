94a7d166ec641678bfe54691c51b1317928adaca
y0ungg <m0_23@naver.com>
2022-10-02 03:12:27 +0900

    refactor: validate and debounce for Signup form input data

--- client/src/views/pages/SignupPage/index.tsx
+++ client/src/views/pages/SignupPage/index.tsx
@@ -1,91 +1,74 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import { AuthWrapper, AuthTitle, AuthForm, AuthLabel, AuthInput, AuthLoginBtn } from 'views/components/login/style';
 import { Content } from 'views/components/UI/molecules/text.style';
 import { Icon } from 'views/components/icons';
-import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import useSignupFlows from './useSignupFlows';
+import useSelectorTyped from 'utils/useSelectorTyped';
+import debouncingChanges from './debouncingChanges';
 
 const Signup = () => {
+  const { email, username, password, isValidEmail, isValidPassword, isValidUserName, error } = useSelectorTyped((state) => state.form.signup_form);
   const navigate = useNavigate();
-  const [isCheckedName, setIsCheckedName] = useState(true);
-  const [isCheckedPw, setIsCheckedPw] = useState(true);
-  const [inputError, setInputError] = useState(false);
-  const [userInfo, setUserInfo] = useState({
-    email: '',
-    username: '',
-    password: ''
-  });
+  const { doSignup } = useSignupFlows();
+  const { onChange } = debouncingChanges();
 
-  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputError(false);
-    if (e.currentTarget.id === 'username') {
-      setIsCheckedName(false);
-      if (e.currentTarget.value.length >= 2) {
-        setIsCheckedName(true);
-      }
-    }
-    if (e.currentTarget.id === 'password') {
-      setIsCheckedPw(false);
-      if (e.currentTarget.value.length >= 8) {
-        setIsCheckedPw(true);
-      }
-    }
-    setUserInfo({ ...userInfo, [e.currentTarget.id]: e.currentTarget.value });
-  };
-
-  const onSubmitSignUp = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSignup = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (isCheckedName && isCheckedPw) {
-      // 회원가입에 성공했습니다 모달
-      axios.post('https://api.cactus-villeage.com/api/v1/members/signup', userInfo)
-        .then(() => navigate('/login'))
-        .catch(err => console.log(err));
-    } else {
-      setInputError(true);
-    }
+    console.log({ email, username, password });
+    void doSignup();
   };
 
   return (
-  <AuthWrapper>
-    <AuthTitle>회원가입</AuthTitle>
-    <AuthForm onSubmit={onSubmitSignUp}>
-      <Content.Check onClick={() => navigate('/login')}>
-        <Icon.LeftArrow />
-        로그인
-      </Content.Check>
-      <AuthLabel htmlFor="email">이메일</AuthLabel>
-      <AuthInput
-        id="email"
-        type="email"
-        required
-        onChange={onChangeInput}
-      />
-      <AuthLabel htmlFor="password">비밀번호</AuthLabel>
-      <AuthInput
-        autoComplete="false"
-        id="password"
-        type="password"
-        required
-        maxLength={20}
-        onChange={onChangeInput}
-      />
-      {isCheckedPw
-        ? null
-        : <Content.Check>
-          비밀번호는 8자 이상 20자 이하로 작성해주세요.
-          </Content.Check>
-      }
-      {inputError
-        ? <Content.Error>
-          가입 실패 : 입력 정보를 확인해주세요.
-          </Content.Error>
-        : null
-      }
-      <AuthLoginBtn type="submit">
-        가입하기
-      </AuthLoginBtn>
-    </AuthForm>
-  </AuthWrapper>
+    <AuthWrapper>
+      <AuthTitle>회원가입</AuthTitle>
+      <AuthForm onSubmit={handleSignup}>
+        <Content.Check onClick={() => navigate('/login')}>
+          <Icon.LeftArrow />
+          로그인
+        </Content.Check>
+        <AuthLabel htmlFor="email">이메일</AuthLabel>
+        <AuthInput
+          id="email"
+          type="email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <Content.Check>
+          {isValidEmail ? '' : '이메일 양식을 확인해주세요.'}
+        </Content.Check>
+        <AuthLabel htmlFor="username">닉네임</AuthLabel>
+        <AuthInput
+          id="username"
+          type="text"
+          required
+          maxLength={8}
+          onChange={onChange}
+        />
+        <Content.Check>
+          {isValidUserName ? '' : '닉네임의 길이는 2자 이상 8자 이하입니다.'}
+        </Content.Check>
+        <AuthLabel htmlFor="password">비밀번호</AuthLabel>
+        <AuthInput
+          autoComplete="false"
+          id="password"
+          type="password"
+          required
+          maxLength={20}
+          onChange={onChange}
+        />
+        <Content.Check>
+          {isValidPassword ? '' : '비밀번호의 길이는 8자 이상 20자 이상입니다.'}
+        </Content.Check>
+        <Content.Error>
+          {error === '' ? '' : '가입 실패 : 입력 정보를 확인해주세요.'}
+        </Content.Error>
+        <AuthLoginBtn type="submit">
+          가입하기
+        </AuthLoginBtn>
+      </AuthForm>
+    </AuthWrapper>
   );
 };
 
