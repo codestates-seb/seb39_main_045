c10cb344d8eec45f89d4f4d1d94e965605fe57e0
민영 <104131962+y0ungg@users.noreply.github.com>
2022-10-02 23:56:26 +0900

    refactor: validate and debounce for Signup form input data (#158)
    * refactor: validate and debounce for Signup form input data
    
    * refactor: validate and debounce for Signup form input data

--- client/src/views/pages/SignupPage/useSignupFlows.tsx
+++ client/src/views/pages/SignupPage/useSignupFlows.tsx
@@ -1,4 +1,22 @@
 import React from 'react';
+import { postSignup } from 'utils/memberApis';
+import useSelectorTyped from 'utils/useSelectorTyped';
+import { useNavigate } from 'react-router-dom';
+import { setSignupError } from 'feature/form';
 
-// const useSignupFlows = () => {
-// };
+const useSignupFlows = () => {
+  const navigate = useNavigate();
+  const { email, username, password } = useSelectorTyped((state) => state.form.signup_form);
+
+  const doSignup = async () => {
+    if (email === '' || username === '' || password === '') return false;
+
+    const { data, status } = await postSignup({ email, username, password });
+    if (status < 300) navigate('/login');
+    else setSignupError(data.message);
+  };
+
+  return { doSignup };
+};
+
+export default useSignupFlows;
