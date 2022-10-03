be64f04f72c32a78c766bf2a64a59beb88fd695b
Jae Hwan Park <75547467+jphwany@users.noreply.github.com>
2022-10-03 21:03:59 +0900

    feat: s3 image download for client to response, such as image binary file (#193)

--- server/server/src/main/java/com/cactusvilleage/server/challenge/service/S3Service.java
+++ server/server/src/main/java/com/cactusvilleage/server/challenge/service/S3Service.java
@@ -1,26 +1,37 @@
 package com.cactusvilleage.server.challenge.service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.util.IOUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
 @Slf4j
-public class S3Upload {
+public class S3Service {
 
     @Value("${aws.s3.bucket}")
     private String bucket;
     private final AmazonS3 amazonS3;
 
     public String upload(MultipartFile multipartFile) throws IOException {
+
         // S3에 저장되는 파일 이름 중복 방지 UUID 랜덤 값 + 파일 이름
         String s3FileName = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
         log.info("[CREATED] created fileName is {}", s3FileName);
@@ -39,5 +50,27 @@ public class S3Upload {
         log.debug("[CREATED] created amazonS3 system is {}", amazonS3);
 
         return amazonS3.getUrl(bucket, s3FileName).toString();
+    }
+
+    public ResponseEntity<byte[]> download(String storedFileName) throws IOException {
+
+        // S3Object 선언 후 s3 버킷, 파일 이름을 전달하고 매칭되면 object 반환 -> 할당
+        S3Object object = amazonS3.getObject(bucket, storedFileName);
+
+        // InputStream 생성, 앞서 만든 object에서 getObjectContent()를 호출해 할당
+        S3ObjectInputStream objectInputStream = object.getObjectContent();
+
+        byte[] bytes = IOUtils.toByteArray(objectInputStream);
+
+        String fileName = URLEncoder.encode(storedFileName, StandardCharsets.UTF_8).replaceAll("\\+", "%20");
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        httpHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        httpHeaders.setContentLength(bytes.length);
+        httpHeaders.setContentDispositionFormData("attachment", fileName);
+
+        return new ResponseEntity<>(bytes, httpHeaders, HttpStatus.OK);
+
     }
 }
