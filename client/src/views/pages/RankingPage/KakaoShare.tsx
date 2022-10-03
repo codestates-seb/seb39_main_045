010ec0ba9d05783dcc32c654c4b7af3c9b4db83c
y0ungg <m0_23@naver.com>
2022-10-03 19:17:58 +0900

    fix: setting up kakao env variables for github actions

--- client/src/views/pages/RankingPage/KakaoShare.tsx
+++ client/src/views/pages/RankingPage/KakaoShare.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { KakaoBtn } from './RankingsPage.style';
+import useSelectorTyped from 'utils/useSelectorTyped';
 
 const KakaoShare = () => {
+  const { username } = useSelectorTyped(state => state.user.userInfo);
   const handleShareBtn = () => {
     window.Kakao.Share.sendCustom({
       templateId: 83427,
       templateArgs: {
-        description: '벌써 0번의 챌린지에 성공한 ㅇㅇㅇㅇ님이 선인장 키우기에 초대하셨어요.'
+        description: `벌써 0번의 챌린지에 성공한 ${username}님이 선인장 키우기에 초대하셨어요.`
       }
     });
   };
@@ -14,9 +16,9 @@ const KakaoShare = () => {
   return (
     <KakaoBtn id="kakaotalk-sharing-btn" onClick={handleShareBtn}>
       <img
-      width={20}
-      src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png"
-      alt="카카오톡 공유 보내기 버튼"
+        width={20}
+        src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png"
+        alt="카카오톡 공유 보내기 버튼"
       />
       <span>카카오톡</span>
     </KakaoBtn>
