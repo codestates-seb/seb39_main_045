cd2c881ef187fbeba1b5920837a92429a941c7f4
romcanrom <baros2san@gmail.com>
2022-10-03 14:04:17 +0900

    feat: enable spring boot application to use batch and schedueler

--- server/server/src/main/java/com/cactusvilleage/server/ServerApplication.java
+++ server/server/src/main/java/com/cactusvilleage/server/ServerApplication.java
@@ -1,24 +1,19 @@
 package com.cactusvilleage.server;
 
-import com.cactusvilleage.server.challenge.entities.Status;
-import com.cactusvilleage.server.challenge.repository.ChallengeRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
 @EnableJpaAuditing
 @EnableBatchProcessing
+@EnableScheduling
 @SpringBootApplication
 public class ServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
-
     }
-
 }
