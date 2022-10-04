5a991fd3c98182656e3e03b801c8b38daa37f7e2
y0ungg <m0_23@naver.com>
2022-10-04 18:10:15 +0900

    fix: delete unused module for build error

--- client/src/views/pages/NotFoundPage/index.tsx
+++ client/src/views/pages/NotFoundPage/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import image from 'assets/img/caclogo.jpg';
 import { Layout } from 'views/components/UI/Layout.style';
-import { Content } from 'views/components/UI/molecules/text.style';
 
 const NotFoundPage = () => {
   return (
