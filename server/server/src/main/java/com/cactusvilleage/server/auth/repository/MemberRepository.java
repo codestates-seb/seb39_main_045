7e2c4f970b7b3194696042b9e4b19d9e37a57b4c
romcanrom <85007104+romcanrom@users.noreply.github.com>
2022-10-05 00:39:42 +0900

    fix: alternative ranking response added when ranker is not present (#230)

--- server/server/src/main/java/com/cactusvilleage/server/auth/repository/MemberRepository.java
+++ server/server/src/main/java/com/cactusvilleage/server/auth/repository/MemberRepository.java
@@ -1,8 +1,10 @@
 package com.cactusvilleage.server.auth.repository;
 
 import com.cactusvilleage.server.auth.entities.Member;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -12,5 +14,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
     boolean existsByUsername(String username);
     boolean existsByProviderId(String providerId);
+    List<Member> findAllByDeleted(boolean deleted, Sort sort);
 
 }
