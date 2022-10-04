f7756b6dcf08c2828685f079cd5e9ae46741e5e8
romcanrom <baros2san@gmail.com>
2022-10-05 05:12:14 +0900

    feat: ranking cal logic fix

--- server/server/src/main/java/com/cactusvilleage/server/challenge/service/ChallengeService.java
+++ server/server/src/main/java/com/cactusvilleage/server/challenge/service/ChallengeService.java
@@ -241,16 +241,22 @@ public class ChallengeService {
 
                 for (Member member : members) {
                     for (int i = 0; i < validRankers.size(); i++) {
-                        if (!member.getUsername().equals(validRankers.get(i).getUsername())) {
+                        boolean flag = member.getUsername().equals(validRankers.get(i).getUsername());
+                        if (flag) {
+                            break;
+                        } else {
                             RankingResponseDto.Rankers dummy = RankingResponseDto.Rankers.builder()
                                     .rank(validRankers.size() + 1)
                                     .username(member.getUsername())
                                     .stamps(0)
                                     .build();
                             validRankers.add(dummy);
-                            break;
+                            if (validRankers.size() == rankerSize) {
+                                break;
+                            }
                         }
                     }
+
                     if (validRankers.size() == rankerSize) {
                         break;
                     }
@@ -287,12 +293,8 @@ public class ChallengeService {
         }
 
         if (collect.size() < index) {
-            List<Member> members = memberRepository.findAllByDeleted(false, Sort.by(Sort.Direction.ASC, "id"));
-
-            int myRank = members.indexOf(member) + 1;
-
             return RankingResponseDto.MyRanking.builder()
-                    .rank(myRank)
+                    .rank(RANKER_SIZE + 1)
                     .username(member.getUsername())
                     .stamps(0)
                     .build();
