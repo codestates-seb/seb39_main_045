a9d1faa6fc55b63443cb99d0f6e3944af70e876b
민영 <104131962+y0ungg@users.noreply.github.com>
2022-10-06 10:31:38 +0900

    fix: change target data variable for Previous Challenges (#279)
    * refactor: redirect expired user, hide nav for MyPages
    
    * fix: change target data variable for Previous Challenges
    
    * fix:  filtering target data for Previous Challenges

--- client/src/utils/selectors.ts
+++ client/src/utils/selectors.ts
@@ -4,7 +4,7 @@ import { RootState } from 'store/store';
 const selectChallenge = (state: RootState) => state.previousChallenges;
 
 export const selectDiaryTypeChallenge = createSelector(selectChallenge, (state) =>
-  state.challenges?.filter((chall) => chall.type === 'diary')
+  state.challenges?.filter((chall) => chall.type === 'thanks')
 );
 
 export const selectStudyTypeChallenge = createSelector(selectChallenge, (state) =>
