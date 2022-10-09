38302013b98ce62ab9e93e45b6c74086f579abc0
sweesweett <sweesweet@naver.com>
2022-10-09 19:37:39 +0900

    fix:change url google sns login for client

--- client/src/views/components/login/GoogleLogin.tsx
+++ client/src/views/components/login/GoogleLogin.tsx
@@ -3,7 +3,7 @@ import google from 'assets/img/btn_google_signin_light_normal_web.png';
 const GoogleLogin = () => {
   const handleGoogle = () => {
     window.location.href =
-      'https://api.cactus-villeage.com/oauth2/authorization/google?redirect_uri=https://dev.cactus-villeage.com/main/';
+      'https://api.cactus-villeage.com/oauth2/authorization/google?redirect_uri=https://www.cactus-villeage.com/main/';
   };
 
   return <img src={google} onClick={handleGoogle} alt="googleLogin" />;
