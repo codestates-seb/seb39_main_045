c577b09396a057711dacdf65e1da121ceeb20f91
sweesweett <sweesweet@naver.com>
2022-10-09 13:35:57 +0900

    feat:explanation modal for mainpage

--- client/src/views/components/mainpageModal/ExplanationModal.tsx
+++ client/src/views/components/mainpageModal/ExplanationModal.tsx
@@ -25,38 +25,38 @@ const ExplanationModal = ({ setIsOpen }: DefaultProps) => {
             close
           </button>
           <Cards>
-            <Card active={active === 0} key={0}>
+            <Card active={active === 0}>
               선인장키우기에 대해 설명해 드릴게요!
             </Card>
-            <Card active={active === 1} key={1}>
+            <Card active={active === 1}>
               먼저 하단에 웃는 이모지는 마이페이지로 이동할 수 있어요.
             </Card>
-            <Card active={active === 2} key={2}>
+            <Card active={active === 2}>
               <MainBtn.choose>챌린지 선택하기</MainBtn.choose>
               <p>버튼을 클릭하신 후 챌린지를 선택해주세요</p>
             </Card>
-            <Card active={active === 3} key={3}>
+            <Card active={active === 3}>
               감사, 기상, 공부 챌린지를 선택할 수 있습니다.
             </Card>
-            <Card active={active === 4} key={4}>
+            <Card active={active === 4}>
               챌린지를 선택하면 아기 선인장을 받으실 수 있습니다.
               <br />
               챌린지 성공도에 따라 선인장이 무럭무럭 자랄거에요!
             </Card>
-            <Card active={active === 5} key={5}>
+            <Card active={active === 5}>
               <MainBtn.water className="material-icons">
                 water_drop
               </MainBtn.water>
               <p>클릭하면 응원의 메시지를 받으실 수 있어요!</p>
             </Card>
-            <Card active={active === 6} key={6}>
+            <Card active={active === 6}>
               <MainBtn.today className="material-icons">wb_sunny</MainBtn.today>
               <p>
                 햇빛 버튼은 일일 챌린지를 등록하고 싶으실 때 폼을 여는
                 버튼이랍니다.
               </p>
             </Card>
-            <Card active={active === 7} key={7}>
+            <Card active={active === 7}>
               설명이 잘 되었을까요? 이제 새로운 챌린지를 시작해보세요!
             </Card>
           </Cards>
