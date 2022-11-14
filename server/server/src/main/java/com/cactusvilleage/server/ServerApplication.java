9405cad8732cd3190157d1038ea429fb74b2c05d
romcanrom <85007104+romcanrom@users.noreply.github.com>
2022-11-14 21:32:04 +0900

    feat: redis not caching null value when expired (by ttl) (#319)

--- server/server/src/main/java/com/cactusvilleage/server/ServerApplication.java
+++ server/server/src/main/java/com/cactusvilleage/server/ServerApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 
+@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 @EnableCaching
 @EnableJpaAuditing
 @EnableBatchProcessing
